514e34dd8aeef1de13e9dad3ed61f10c864ec274
Ardalan Amini <ardalanamini22@gmail.com>
2022-07-25 15:11:14 +0430

    docs: Update documents

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { default, default as MiniCSS } from "./MiniCSS.js";
 export { default as Node } from "./Node.js";
 export {
+  type NodeJSONOutputI,
   type MiniCSSJSONOutputI,
   type NodeAttributeSelectorI,
   type AttributeSelectorI,
